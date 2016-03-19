70178179a36367277672987d6761dab4ed85a197
arshvid <arshameslami@gmail.com>
2016-03-19 02:28:55 -0400

    19 - 03 - 2016 v2
    PHP Seperate file

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+
+if(isset($_POST['send'])) {
+   // Prepare the email
+$to = 'designcampstudios@gmail.com';
+
+$name = $_POST['name'];
+$mail_from = $_POST['email'];
+   $subject = 'Summer Camp Inquiry';
+   $message = $_POST['message'];
+
+$header = "From: $name <$mail_from>";
+
+   // Send it
+   $sent = mail($to, $subject, $message, $header);
+   if($sent) {
+   echo 'Your message has been sent successfully!';
+   } else {
+   echo 'Sorry, your message could not send.';
+   }
+}
+
+?>
